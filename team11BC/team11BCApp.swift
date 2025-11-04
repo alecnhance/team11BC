9784117e605a85205f1d14d4affd78c793cbcdc9
Alec Hance <alecnhance@gmail.com>
2025-11-03 20:35:23 -0500

    initial commit

---
+++ team11BC/team11BCApp.swift
@@ -0,0 +1,17 @@
+//
+//  team11BCApp.swift
+//  team11BC
+//
+//  Created by Alec Hance on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct team11BCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ team11BCTests/team11BCTests.swift
@@ -0,0 +1,17 @@
+//
+//  team11BCTests.swift
+//  team11BCTests
+//
+//  Created by Alec Hance on 11/3/25.
+//
+
+import Testing
+@testable import team11BC
+
+struct team11BCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
