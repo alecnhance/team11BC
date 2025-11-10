21db8e012d5f8e457816d8f87206bdcbc094770d
adi24-7 <aditigd1@gmail.com>
2025-11-09 22:15:40 -0500

    Created the intro page

--- team11BC/ContentView.swift
+++ team11BC/ContentView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                Color(red: 39.0 / 255.0, green: 76.0 / 255.0, blue: 119.0 / 255.0)
+                    .ignoresSafeArea()
+                HStack{
+                    Text("Lost and Found App")
+                        .foregroundColor(.white)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 100)
+                        .font(.title)
+                }
+                NavigationLink(destination: InventoryTracker()) {
+                    Text("Continue as a guest")
+                        .foregroundColor(.white)
+                        .font(.body)
+                }
+            }
+        }.navigationTitle("Main View")
     }
 }
-
 #Preview {
     ContentView()
 }
