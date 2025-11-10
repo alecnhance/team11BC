6072071db794b11b575feeace9af1e0bafe9482b
aim-eeeeeeee <azheng625@gmail.com>
2025-11-10 13:02:37 -0500

    Update Navigation.swift

---
+++ team11BC/Navigation.swift
@@ -0,0 +1,48 @@
+//
+//  Navigation.swift
+//  team11BC
+//
+//  Created by Aimee Zheng on 2025/11/10.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            // Inventory (Home) Page
+            NavigationView {
+                InventoryView()
+                    .navigationTitle("Homw")
+            }
+            .tabItem {
+                Image(systemName: "list.bullet")
+                Text("Inventory")
+            }.tint(.blue)
+            
+            // Report Lost Item Page
+            NavigationView {
+                LostAndFoundView()
+                    .navigationTitle("Report Lost")
+            }
+            .tabItem {
+                Image(systemName: "exclamationmark.bubble")
+                Text("Report Lost")
+            }.tint(.blue)
+           
+            // Bounty Page
+            NavigationView {
+                Text("Bounty Page")
+                    .navigationTitle("Bounties")
+            }
+            .tabItem {
+                Image(systemName: "doc.richtext")
+                Text("Bounties")
+            }.tint(.blue)
+        }
+        }
+}
+
+#Preview {
+    MainTabView()
+}
