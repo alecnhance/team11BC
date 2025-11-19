c3dd8e1685498e4185abbb8349684c7d65215bac
maymayp <139157164+maymayp@users.noreply.github.com>
2025-11-18 23:07:05 -0500

    Add files via upload

--- team11BC/team11BCApp.swift
+++ team11BC/team11BCApp.swift
@@ -5,13 +5,39 @@
 //  Created by Alec Hance on 11/3/25.
 //
 
+//import SwiftUI
+//import FirebaseCore
+//@main
+//struct team11BCApp: App {
+//    init() {
+//        FirebaseApp.configure()
+//    }
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
+
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct team11BCApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+
+                ContentView()
         }
     }
 }
